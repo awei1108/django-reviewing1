42c1b71372bddaf602da8a20cb717844fc397720
awei1108 <awei1108@pchome.com.tw>
2015-07-30 10:20:16 +0800

    First commit

---
+++ hehesite/hehesite/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+# import view functions from trips app
+from heheapp.views import hello_world, home, post_detail
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hehesite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello_world),
+    url(r'^$', home),
+    url(r'^post/(?P<id>\d+)/$', post_detail, name='post_detail'),
+)
+
+
